d7e69ae7838bdd555da7b08b992d4087705c7b03
AVPavlov <avpavlov@lanit.ru>
2024-12-13 07:54:56 +0300

    Lab3

--- Lab3/Lab3/source1.c
+++ Lab3/Lab3/source1.c
@@ -1,41 +1,332 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <windows.h>
-#define RENAMING_SUCCESS "File renamed successfully\n"
-#define RENAMING_ERROR_MSG "Error while renaming file with code: %lu\n"
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_INCREMENT 256
+#define MAX_INPUT_SIZE 256
+#define ERROR_MSG "ERROR!\n"
+
+void clear_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+char* read_input() {
+    size_t bufferSize = BUFFER_INCREMENT;
+    char* buffer = (char*)malloc(bufferSize);
+    if (buffer == NULL) {
+        printf(ERROR_MSG);
+        return NULL;
+    }
+
+    size_t total = 0;
+    char tempBuffer[MAX_INPUT_SIZE];
+
+    while (1) {
+        if (fgets(tempBuffer, sizeof(tempBuffer), stdin) == NULL) {
+            break;
+        }
+
+        if (tempBuffer[0] == '\n') {
+            break;
+        }
+
+        size_t tempLength = strlen(tempBuffer);
+        if (total + tempLength >= bufferSize) {
+            
+            bufferSize += BUFFER_INCREMENT;
+            char* newBuffer = (char*)realloc(buffer, bufferSize);
+            if (newBuffer == NULL) {
+                printf(ERROR_MSG);
+                free(buffer);
+                return NULL;
+            }
+            buffer = newBuffer;
+        }
+
+        strcpy(buffer + total, tempBuffer);
+        total += tempLength;
+    }
+
+    buffer[total] = '\0';
+
+    return buffer;
+}
 
 void print_help() {
-    printf("Info: rename_file <old_file_name> <new_file_name>\n");
-    printf("Rename file from <old_file_name> to <new_file_name>\n");
+    printf("INFO:\n");
+    printf("create <file>\n");
+    printf("read <file>\n");
+    printf("rename <oldfile> <newfile>\n");
+    printf("delete <file>\n");
+    printf("copy <src> <dst>\n");
+    printf("size <filename>\n");
+    printf("attrib <filename>\n");
+    printf("readonly <filename>\n");
+    printf("hidden <filename>\n");
+    printf("'dir' <path>\n");
+    printf("'help'\n");
+    printf("'exit'\n");
+}
+
+void create_file(const char* filename) {
+    HANDLE hFile;
+    DWORD bytesWritten;
+
+    char* buffer = read_input();
+    if (buffer == NULL) {
+        return;
+    }
+
+    hFile = CreateFile(
+        filename,
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("ERROR: %d\n", GetLastError());
+        free(buffer);
+        return;
+    }
+
+    if (!WriteFile(hFile, buffer, (DWORD)strlen(buffer), &bytesWritten, NULL)) {
+        printf("Ошибка записи в файл: %d\n", GetLastError());
+    }
+    else {
+        printf("Записано %lu байт в файл %s\n", bytesWritten, filename);
+    }
+
+    CloseHandle(hFile);
+    free(buffer);
+}
+
+
+
+void read_file(const char* filename) {
+    HANDLE hFile = CreateFileA(
+        filename,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+    
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf(ERROR_MSG);
+        return;
+    }
+
+    char buffer[10];
+    DWORD bytesRead;
+    printf("File content:\n");
+    while (TRUE) {
+        if (ReadFile(hFile, buffer, 9, &bytesRead, NULL) == FALSE) {
+            printf("ERROR: %d\n", GetLastError());
+            CloseHandle(hFile);
+            return;
+        }
+
+        if (bytesRead == 0) {
+            break;
+        }
+
+        buffer[bytesRead] = '\0';
+        printf("%s", buffer);
+    }
+
+    CloseHandle(hFile);
+}
+
+void delete_file(const char* filename) {
+    if (DeleteFile(filename)) {
+        printf("Файл удален\n");
+    }
+    else {
+        printf("Ошибка удаления файла\n");
+    }
+}
+
+void rename_file(const char* oldname, const char* newname) {
+    if (MoveFile(oldname, newname)) {
+        printf("Файл переименован\n");
+    }
+    else {
+        printf("Ошибка переименования файла\n");
+    }
+}
+
+void copy_file(const char* src, const char* dst) {
+    if (CopyFile(src, dst, FALSE)) {
+        printf("Файл скопирован\n");
+    }
+    else {
+        printf("Ошибка копирования файла\n");
+    }
+}
+
+void file_size(const char* filename) {
+    HANDLE hFile = CreateFile(
+        filename,
+        GENERIC_READ,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+    
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf(ERROR_MSG);
+        return;
+    }
+
+    DWORD size = GetFileSize(hFile, NULL);
+    printf("File size: %d байт\n", size);
+    CloseHandle(hFile);
+}
+
+void file_attrib(const char* filename) {
+    DWORD attributes = GetFileAttributes(filename);
+    if (attributes == INVALID_FILE_ATTRIBUTES) {
+        printf("Ошибка получения атрибутов файла\n");
+        return;
+    }
+
+    printf("Атрибуты файла: ");
+    if (attributes & FILE_ATTRIBUTE_READONLY) printf("Только для чтения ");
+    if (attributes & FILE_ATTRIBUTE_HIDDEN) printf("Скрытый ");
+    if (attributes & FILE_ATTRIBUTE_SYSTEM) printf("Системный ");
+    if (attributes & FILE_ATTRIBUTE_ARCHIVE) printf("Архив ");
+    printf("\n");
+}
+
+void set_readonly(const char* filename) {
+    DWORD attributes = GetFileAttributes(filename);
+    if (attributes == INVALID_FILE_ATTRIBUTES) {
+        printf("Ошибка получения атрибутов файла\n");
+        return;
+    }
+
+    attributes |= FILE_ATTRIBUTE_READONLY;
+    if (SetFileAttributes(filename, attributes)) {
+        printf("Атрибут 'только для чтения' установлен\n");
+    }
+    else {
+        printf("Ошибка установки атрибута\n");
+    }
+}
+
+void set_hidden(const char* filename) {
+    DWORD attributes = GetFileAttributes(filename);
+    if (attributes == INVALID_FILE_ATTRIBUTES) {
+        printf("Ошибка получения атрибутов файла\n");
+        return;
+    }
+
+    attributes |= FILE_ATTRIBUTE_HIDDEN;
+    if (SetFileAttributes(filename, attributes)) {
+        printf("Атрибут 'скрытый' установлен\n");
+    }
+    else {
+        printf("Ошибка установки атрибута\n");
+    }
+}
+
+void list_directory(const char* path) {
+    WIN32_FIND_DATA findData;
+
+    char pathWithWildcard[MAX_PATH];
+    strcpy(pathWithWildcard, path);
+    strcat(pathWithWildcard, "\\*");
+
+    HANDLE hFind = FindFirstFile(pathWithWildcard, &findData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        printf("Ошибка открытия директории\n");
+        return;
+    }
+
+    do {
+        printf("%s\n", findData.cFileName);
+    } while (FindNextFile(hFind, &findData) != 0);
+
+    FindClose(hFind);
+}
+
+
+void process_command(char* command) {
+    char cmd[256], arg1[256], arg2[256];
+    int args = sscanf(command, "%s %s", cmd, arg1);
+    if (strcmp(cmd, "create") == 0) {
+        create_file(arg1);
+    }
+    else if (strcmp(cmd, "read") == 0) {
+        read_file(arg1);
+    }
+    else if (strcmp(cmd, "delete") == 0) {
+        delete_file(arg1);
+    }
+    else if (strcmp(cmd, "rename") == 0) {
+        args = sscanf(command, "%s %s %s", cmd, arg1, arg2);
+        rename_file(arg1, arg2);
+    }
+    else if (strcmp(cmd, "copy") == 0) {
+        args = sscanf(command, "%s %s %s", cmd, arg1, arg2);
+        copy_file(arg1, arg2);
+    }
+    else if (strcmp(cmd, "size") == 0) {
+        file_size(arg1);
+    }
+    else if (strcmp(cmd, "attrib") == 0) {
+        file_attrib(arg1);
+    }
+    else if (strcmp(cmd, "readonly") == 0) {
+        set_readonly(arg1);
+    }
+    else if (strcmp(cmd, "hidden") == 0) {
+        set_hidden(arg1);
+    }
+    else if (strcmp(cmd, "dir") == 0) {
+        list_directory(arg1);
+    }
+    else if (strcmp(cmd, "help") == 0) {
+        print_help();
+    }
+    else if (strcmp(cmd, "exit") == 0) {
+        exit(0);
+    }
+    else {
+        printf("Неизвестная команда. Введите 'help' для получения списка команд.\n");
+    }
 }
 
 int main(int argc, char* argv[]) {
+    SetConsoleOutputCP(CP_UTF8);
     if (argc == 1) {
-        char old_file_name[MAX_PATH], new_file_name[MAX_PATH];
-        printf("Enter filename: ");
-        scanf("%s", old_file_name);
-        printf("Enter new filename: ");
-        scanf("%s", new_file_name);
-
-        if (MoveFile(old_file_name, new_file_name)) {
-            printf(RENAMING_SUCCESS);
-        }
-        else {
-            printf(RENAMING_ERROR_MSG, GetLastError());
-            return -1;
-        }
-    }
-    else if (argc == 3) {
-        if (MoveFile(argv[1], argv[2])) {
-            printf(RENAMING_SUCCESS);
-        }
-        else {
-            printf(RENAMING_ERROR_MSG, GetLastError());
-            return -1;
+        char command[256];
+        print_help();
+        while (1) {
+            printf("\nВведите команду: ");
+            fgets(command, sizeof(command), stdin);
+            command[strcspn(command, "\n")] = 0;
+            process_command(command);
         }
     }
     else {
-        print_help();
+        char command[256] = "";
+        for (int i = 1; i < argc; ++i) {
+            strcat(command, argv[i]);
+            strcat(command, " ");
+        }
+        process_command(command);
     }
 
     return 0;
--- Lab3/Lab3/source2.c
+++ Lab3/Lab3/source2.c
@@ -1,65 +1,267 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <string.h>
 
-void print_help() {
-    printf("Info: remove_a <filename>\n");
-    printf("Remove all occurrences of 'a' from the file and append the count of removed 'a' in end\n");
+#define BUFFER_SIZE 256
+
+int compare_desc(const void* a, const void* b) {
+    int num1 = *(int*)a;
+    int num2 = *(int*)b;
+    return (num2 - num1);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        print_help();
-        return 0;
-    }
+int compare_letters(const void* a, const void* b) {
+    return (*(char*)a - *(char*)b);
+}
 
-    HANDLE hFile = CreateFile(argv[1], GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+void display_help() {
+    printf("Использование программы:\n");
+    printf("1. sort_letters <filename> - Упорядочить буквы по алфавиту в файле.\n");
+    printf("2. count_letters <filename> - Посчитать количество строчных и прописных букв в файле.\n");
+    printf("3. remove_letter_a <filename> - Удалить все буквы 'а' из файла и записать количество удаленных букв.\n");
+    printf("4. sort_numbers <filename> - Отсортировать содержимое числового файла по убыванию.\n");
+}
+
+void sort_letters(const char* filename) {
+    HANDLE hFile = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hFile == INVALID_HANDLE_VALUE) {
-        printf("Error while opening file: %lu\n", GetLastError());
-        return -1;
+        printf("Ошибка открытия файла.\n");
+        return;
     }
 
     DWORD fileSize = GetFileSize(hFile, NULL);
     if (fileSize == INVALID_FILE_SIZE) {
-        printf("Error while getting file size: %lu\n", GetLastError());
+        printf("Ошибка получения размера файла.\n");
         CloseHandle(hFile);
-        return -1;
+        return;
     }
 
     HANDLE hMapping = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, fileSize, NULL);
     if (hMapping == NULL) {
-        printf("Error while creating file mapping: %lu\n", GetLastError());
+        printf("Ошибка создания отображения файла.\n");
         CloseHandle(hFile);
-        return -1;
+        return;
     }
 
-    char* fileContent = (char*)MapViewOfFile(hMapping, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, fileSize);
+    char* fileContent = (char*)MapViewOfFile(hMapping, FILE_MAP_ALL_ACCESS, 0, 0, fileSize);
     if (fileContent == NULL) {
-        printf("Error while mapping file view: %lu\n", GetLastError());
+        printf("Ошибка отображения файла.\n");
         CloseHandle(hMapping);
         CloseHandle(hFile);
-        return -1;
+        return;
     }
 
-    int counter = 0;
-    for (DWORD i = 0; i < fileSize; i++) {
-        if (fileContent[i] == 'a') {
-            fileContent[i] = ' ';
-            counter++;
-        }
-    }
-
-    char countBuffer[32];
-    sprintf(countBuffer, "\nNumber of 'a' removed: %d", counter);
-    DWORD countSize = strlen(countBuffer);
-    SetFilePointer(hFile, 0, NULL, FILE_END);
-    DWORD written;
-    WriteFile(hFile, countBuffer, countSize, &written, NULL);
+    qsort(fileContent, fileSize, sizeof(char), compare_letters);
 
     UnmapViewOfFile(fileContent);
     CloseHandle(hMapping);
     CloseHandle(hFile);
 
-    printf("Successfully removed %d occurrences of 'a'\n", counter);
+    printf("Сортировка завершена.\n");
+}
+
+void count_letters(const char* filename) {
+    HANDLE hFile = CreateFile(filename, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Ошибка открытия файла.\n");
+        return;
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) {
+        printf("Ошибка получения размера файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    HANDLE hMapping = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, fileSize, NULL);
+    if (hMapping == NULL) {
+        printf("Ошибка создания отображения файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    char* fileContent = (char*)MapViewOfFile(hMapping, FILE_MAP_READ, 0, 0, fileSize);
+    if (fileContent == NULL) {
+        printf("Ошибка отображения файла.\n");
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return;
+    }
+
+    int lowercase = 0, uppercase = 0;
+    for (DWORD i = 0; i < fileSize; i++) {
+        if (fileContent[i] >= 'a' && fileContent[i] <= 'z') {
+            lowercase++;
+        }
+        else if (fileContent[i] >= 'A' && fileContent[i] <= 'Z') {
+            uppercase++;
+        }
+    }
+
+    printf("Строчные буквы: %d\n", lowercase);
+    printf("Прописные буквы: %d\n", uppercase);
+
+    UnmapViewOfFile(fileContent);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+}
+
+void remove_letter_a(const char* filename) {
+    HANDLE hFile = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Ошибка открытия файла.\n");
+        return;
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) {
+        printf("Ошибка получения размера файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    HANDLE hMapping = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, fileSize, NULL);
+    if (hMapping == NULL) {
+        printf("Ошибка создания отображения файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    char* fileContent = (char*)MapViewOfFile(hMapping, FILE_MAP_ALL_ACCESS, 0, 0, fileSize);
+    if (fileContent == NULL) {
+        printf("Ошибка отображения файла.\n");
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return;
+    }
+
+    int removedCount = 0;
+    DWORD j = 0;
+    for (DWORD i = 0; i < fileSize; i++) {
+        if (fileContent[i] != 'a') {
+            fileContent[j++] = fileContent[i];
+        }
+        else {
+            removedCount++;
+        }
+    }
+
+    SetFilePointer(hFile, j, NULL, FILE_BEGIN);
+    SetEndOfFile(hFile);
+
+    printf("Удалено букв 'а': %d\n", removedCount);
+
+    UnmapViewOfFile(fileContent);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+}
+
+void sort_numbers(const char* filename) {
+    HANDLE hFile = CreateFile(filename, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        printf("Ошибка открытия файла.\n");
+        return;
+    }
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) {
+        printf("Ошибка получения размера файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    HANDLE hMapping = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, fileSize, NULL);
+    if (hMapping == NULL) {
+        printf("Ошибка создания отображения файла.\n");
+        CloseHandle(hFile);
+        return;
+    }
+
+    int* fileContent = (int*)MapViewOfFile(hMapping, FILE_MAP_ALL_ACCESS, 0, 0, fileSize);
+    if (fileContent == NULL) {
+        printf("Ошибка отображения файла.\n");
+        CloseHandle(hMapping);
+        CloseHandle(hFile);
+        return;
+    }
+
+    DWORD count = fileSize / sizeof(int);
+    qsort(fileContent, count, sizeof(int), compare_desc);
+
+    printf("Числа отсортированы по убыванию.\n");
+
+    UnmapViewOfFile(fileContent);
+    CloseHandle(hMapping);
+    CloseHandle(hFile);
+}
+
+void interactive_mode() {
+    int choice;
+    char filename[BUFFER_SIZE];
+
+    while (1) {
+        printf("\nВыберите команду:\n");
+        printf("1. Упорядочить буквы по алфавиту\n");
+        printf("2. Посчитать количество строчных и прописных букв\n");
+        printf("3. Удалить все буквы 'а'\n");
+        printf("4. Отсортировать числа по убыванию\n");
+        printf("5. Выход\n");
+        printf("Ваш выбор: ");
+        scanf("%d", &choice);
+
+        if (choice == 5) {
+            break;
+        }
+
+        printf("Введите имя файла: ");
+        scanf("%s", filename);
+
+        switch (choice) {
+        case 1:
+            sort_letters(filename);
+            break;
+        case 2:
+            count_letters(filename);
+            break;
+        case 3:
+            remove_letter_a(filename);
+            break;
+        case 4:
+            sort_numbers(filename);
+            break;
+        default:
+            printf("Неверный выбор!\n");
+            break;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        interactive_mode();
+    }
+    else if (argc == 3) {
+        if (strcmp(argv[1], "sort") == 0) {
+            sort_letters(argv[2]);
+        }
+        else if (strcmp(argv[1], "count") == 0) {
+            count_letters(argv[2]);
+        }
+        else if (strcmp(argv[1], "remove_a") == 0) {
+            remove_letter_a(argv[2]);
+        }
+        else if (strcmp(argv[1], "sort_numbers") == 0) {
+            sort_numbers(argv[2]);
+        }
+        else {
+            display_help();
+        }
+    }
+    else {
+        display_help();
+    }
+
     return 0;
 }
